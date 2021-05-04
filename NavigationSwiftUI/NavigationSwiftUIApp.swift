f231b23a580503303edbffb5ac36633065bc472d
Felipe Carrasco <felipe@bakers.dev>
2021-05-04 18:53:37 -0400

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by Pipe Carrasco on 04-05-21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
